812821e6f489f9b4183550bbb87a18b0e00a54c6
bob-jacobs-jr <bob.jacobs.jr@gmail.com>
2016-04-14 11:56:24 -0400

    Fixed issue with ioapic setting and multiple cpus.  Also, re-worked networking to have two host-only and one NAT.

--- create-node-vm.sh
+++ create-node-vm.sh
@@ -2,10 +2,12 @@
 # DEFAULTS
 DISK_SIZE="10"
 MEMORY="2048"
-OS_TYPE="Ubuntu"
+OS_TYPE="Ubuntu_64"
 STORAGE="50"
 ISO="/home/$USER/ISOs/ubuntu-14.04.2-server-amd64.iso"
-NETWORK="vboxnet0"
+NETWORK1="vboxnet0"
+NETWORK2="vboxnet1"
+CPUS=2
 
 for i in "$@"
 do
@@ -23,6 +25,10 @@ case $i in
     MEMORY="${i#*=}"
     shift # past argument=value
     ;;
+    -c=*|--cpu=*)
+    CPUS="${i#*=}"
+    shift # past argument=value
+    ;;
     -o=*|--os=*)
     OS_TYPE="${i#*=}"
     shift # past argument=value
@@ -31,8 +37,12 @@ case $i in
     STORAGE="${i#*=}"
     shift # past argument=value
     ;;
-    -n=*|--network=*)
-    NETWORK="${i#*=}"
+    -n1=*|--network1=*)
+    NETWORK1="${i#*=}"
+    shift # past argument=value
+    ;;
+    -n2=*|--network2=*)
+    NETWORK2="${i#*=}"
     shift # past argument=value
     ;;
     -i=*|--iso=*)
@@ -57,18 +67,23 @@ then
     echo "VM Host Only Network = ${NETWORK}"
     echo "VM location = ${HOME_DIR}/${NAME}.vbox"
     VBoxManage createvm --name ${NAME} --register
-    VBoxManage createhd --filename "${HOME_DIR}/${NAME}.vdi" --size $(( DISK_SIZE * 1024 ))
-    VBoxManage createhd --filename "${HOME_DIR}/${NAME}_data.vdi" --size $(( STORAGE * 1024 ))
-    VBoxManage modifyvm ${NAME} --ostype ${OS_TYPE}
-    VBoxManage modifyvm ${NAME} --memory ${MEMORY} --vram 128
+    VBoxManage modifyvm ${NAME} --memory ${MEMORY} --vram 128 --cpus ${CPUS}  --ioapic on --ostype ${OS_TYPE}
     VBoxManage storagectl ${NAME} --name "SATA Controller" --add sata --controller IntelAHCI
+    VBoxManage createhd --filename "${HOME_DIR}/${NAME}.vdi" --size $(( DISK_SIZE * 1024 ))
     VBoxManage storageattach ${NAME} --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "${HOME_DIR}/${NAME}.vdi"
-    VBoxManage storageattach ${NAME} --storagectl "SATA Controller" --port 1 --device 0 --type hdd --medium "${HOME_DIR}/${NAME}_data.vdi"
+
+    if [[ $STORAGE -gt 0 ]]
+    then
+        VBoxManage createhd --filename "${HOME_DIR}/${NAME}_data.vdi" --size $(( STORAGE * 1024 ))
+        VBoxManage storageattach ${NAME} --storagectl "SATA Controller" --port 1 --device 0 --type hdd --medium "${HOME_DIR}/${NAME}_data.vdi"
+    fi
+ 
     VBoxManage storagectl ${NAME} --name "IDE Controller" --add ide
     VBoxManage storageattach ${NAME} --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium ${ISO}
     VBoxManage modifyvm ${NAME} --boot1 disk --boot2 dvd --boot3 none --boot4 none
-    VBoxManage modifyvm ${NAME} --nic1 nat --cableconnected1 on
-    VBoxManage modifyvm ${NAME} --nic2 hostonly --hostonlyadapter2 ${NETWORK} --cableconnected2 on
+    VBoxManage modifyvm ${NAME} --nic1 hostonly --hostonlyadapter1 ${NETWORK1} --cableconnected1 on
+    VBoxManage modifyvm ${NAME} --nic2 hostonly --hostonlyadapter2 ${NETWORK2} --cableconnected2 on
+    VBoxManage modifyvm ${NAME} --nic3 nat --cableconnected1 on
 else
     echo "--name parameter is required."
 fi
